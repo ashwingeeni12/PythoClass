a78ff6add03dc0f2cda8ba0853f28387e6c0722c
ashwingeeni12 <99517865+ashwingeeni12@users.noreply.github.com>
2022-07-14 18:14:37 +0000

    Make a Phrase

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print(str(3+2) + ", ", str(3-2) + ", ", str(3*2) + ", ", str(3/2) + ", ", str(3**2) + ", ", str(3//2) + ", ", str(3%2))
+input1 = '"data types."'
+num1 = 5
+num2 = 37
+num3 = 63
+num4 = 212
+num5 = 501
+num6 = 104 
+num7 = 67.8
+num8 = 79.4
+bool1 = True
+bool2 = False
+print("So here are some " + str(input1) + "Variables can store data such as numbers like " + str(num1) + ", " + str(num2) + ", " + str(num3) + ", " + str(num4) + ", " + str(num5) + ", " + str(num6) + ", " + str(num7) + ", " + str(num8) + ", and more. They can also store " + str(bool1) + "/" + str(bool2) + " values.")
