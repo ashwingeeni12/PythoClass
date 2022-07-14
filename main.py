fe10f80947a17e206530129092d796489f5f0b7b
ashwingeeni12 <99517865+ashwingeeni12@users.noreply.github.com>
2022-07-14 18:31:13 +0000

    Supply List

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-input1 = '"data types."'
-num1 = 5
-num2 = 37
-num3 = 63
-num4 = 212
-num5 = 501
-num6 = 104 
-num7 = 67.8
-num8 = 79.4
 bool1 = True
-bool2 = False
-print("So here are some " + str(input1) + "Variables can store data such as numbers like " + str(num1) + ", " + str(num2) + ", " + str(num3) + ", " + str(num4) + ", " + str(num5) + ", " + str(num6) + ", " + str(num7) + ", " + str(num8) + ", and more. They can also store " + str(bool1) + "/" + str(bool2) + " values.")
+string1 = "Main Street Restuarant"
+num1 = 37
+num2 = 24
+num3 = 10
+num4 = 12
+print("Day 1" + '\n' + "Workday - " + str(bool1) + '\n' + "Branch - " + str(string1) + '\n' + "Tomatoes - " + str(num1) + '\n' + "Mangos - " + str(num2) + '\n' + "Bananas - " + str(num3) + '\n' + "Loaves of Bread - " + str(num4) + '\n')
+print("Day 2" + '\n' + "Workday - " + str(bool1) + '\n' + "Branch - " + str(string1) + '\n' + "Tomatoes - " + str(num1 - 5) + '\n' + "Mangos - " + str(num2 - 5) + '\n' + "Bananas - " + str(num3 - 5) + '\n' + "Loaves of Bread - " + str(num4 - 5) + '\n')
+print("Day 3" + '\n' + "Workday - " + str(bool1) + '\n' + "Branch - " + str(string1) + '\n' + "Tomatoes - " + str(num1 - 10) + '\n' + "Mangos - " + str(num2 - 10) + '\n' + "Bananas - " + str(num3 - 10) + '\n' + "Loaves of Bread - " + str(num4 - 10))
