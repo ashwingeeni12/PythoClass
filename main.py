b2c87bd51c37fe593bea063ae94f96a614865935
ashwingeeni12 <99517865+ashwingeeni12@users.noreply.github.com>
2022-07-18 20:54:33 +0000

    Grade List

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-bool1 = True
-string1 = "Main Street Restuarant"
-num1 = 37
-num2 = 24
-num3 = 10
-num4 = 12
-print("Day 1" + '\n' + "Workday - " + str(bool1) + '\n' + "Branch - " + str(string1) + '\n' + "Tomatoes - " + str(num1) + '\n' + "Mangos - " + str(num2) + '\n' + "Bananas - " + str(num3) + '\n' + "Loaves of Bread - " + str(num4) + '\n')
-print("Day 2" + '\n' + "Workday - " + str(bool1) + '\n' + "Branch - " + str(string1) + '\n' + "Tomatoes - " + str(num1 - 5) + '\n' + "Mangos - " + str(num2 - 5) + '\n' + "Bananas - " + str(num3 - 5) + '\n' + "Loaves of Bread - " + str(num4 - 5) + '\n')
-print("Day 3" + '\n' + "Workday - " + str(bool1) + '\n' + "Branch - " + str(string1) + '\n' + "Tomatoes - " + str(num1 - 10) + '\n' + "Mangos - " + str(num2 - 10) + '\n' + "Bananas - " + str(num3 - 10) + '\n' + "Loaves of Bread - " + str(num4 - 10))
+def grader(name, grade):
+    if grade >= 89.5:
+      print(name + " got an A!")
+    elif 89.5 > grade >= 79.5:
+      print(name + " got an B!")
+    elif 79.5 > grade >= 69.5:
+      print(name + " got an C!")
+    else:
+      print(name + " failed :(")
+
+
+#don't change anything below this
+grader("Jack", 79.6)
+grader("Bob", 88.3)
+grader("Alice", 97.6)
+grader("Mysha", 89.4)
+grader("Arjeet", 95.8)
+grader("Billy", 2.1)
